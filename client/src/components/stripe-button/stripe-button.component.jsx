3dbfde334e4971a160e68ac4b67f2b2cf8e38463
Olufeyimi Samuel <donsmog@gmail.com>
2021-12-09 22:05:04 +0100

    stripe button

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -21,7 +21,7 @@ const StripeCheckoutButton = ({ price }) => {
         alert("Payment Successful! Thank you for your purchase.");
       })
       .catch((error) => {
-        // console.log("Payment error: ", JSON.parse(error));
+        console.log("Payment error: ", JSON.parse(error));
         alert(
           "There was an issue with your payment. Please make sure you use the provided credit card."
         );
